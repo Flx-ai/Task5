5cf4dcd413068eab291e2b0a523a241ae9ce7a47
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-11-03 17:05:00 +0300

    Task5

---
+++ src/vsu/cs/ru/num18/Main.java
@@ -0,0 +1,42 @@
+package vsu.cs.ru.num18;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int height = readHeight();
+        System.out.println();
+
+        if (height < 1) {
+            System.out.println("You entered incorrect height. The height must be greater than 1.");
+            return;
+        }
+
+        drawFigure(height);
+    }
+
+    private static int readHeight() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Input height: ");
+        return scanner.nextInt();
+    }
+
+    private static void drawFigure(int height) {
+        int lengthOfString = height;
+
+        for (int row = 0; row <= height; row++) {
+            for (int characterPosition = 0; characterPosition < lengthOfString; characterPosition++) {
+                if (row % 2 == 0 && characterPosition % 2 == 0 && characterPosition >= row) {
+                    System.out.print("\\");
+                    continue;
+                }
+                if (row % 2 != 0 && characterPosition % 2 != 0 && characterPosition >= row) {
+                    System.out.print("\\");
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
